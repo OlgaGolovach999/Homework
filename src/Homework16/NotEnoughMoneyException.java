0ccad37e8082266b174ad898be2d298264392077
OlgaGolovach999 <renato4kabox@gmail.com>
2025-04-09 20:50:23 +0300

    Homework16

---
+++ src/Homework16/NotEnoughMoneyException.java
@@ -0,0 +1,23 @@
+package Homework16;
+
+public class NotEnoughMoneyException extends RuntimeException {
+    private String errorCode;
+
+    public NotEnoughMoneyException(String message, String errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public String toString() {
+        return "NotEnoughMoneyException{errorCode='" + errorCode + "', message='" + getMessage() + "'}";
+    }
+}
